34c1accd4c2bb9512311aaf9f36ebcfe27873428
Humaun Kabir <psychopath.humaun@gmail.com>
2020-07-13 15:48:00 +0600

    Change config to deploy

---
+++ precache-manifest.51872044535f9d561eb01416b914e826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd679a8b80d89e179c9c035f7a142936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e45c6a238d4937847d8",
+    "url": "/static/css/main.553c6a4a.chunk.css"
+  },
+  {
+    "revision": "76ca926d55b86d064f03",
+    "url": "/static/js/2.0a6d994a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0a6d994a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e45c6a238d4937847d8",
+    "url": "/static/js/main.5ead6d51.chunk.js"
+  },
+  {
+    "revision": "0874760245d0efd4d7b9",
+    "url": "/static/js/runtime-main.d8efef39.js"
+  }
+]);
